99f54fc537bea201558e557f64216c0943b31a51
TBSDrJ <marcus.jaiclin@bishops.com>
2020-06-17 16:29:07 -0700

    Fix bug in changeAmplitude. Implement mult. file input/output.

--- wavDataAug.py
+++ wavDataAug.py
@@ -5,6 +5,8 @@ import wave
 from struct import pack, unpack
 # Module to generate random values for data augmentation; in Standard Library
 from random import randrange
+# Module to run system commands, use to get directory listing; in Standard Library
+from subprocess import run
 
 """
 Assumption: A .wav file (assuming mono not stereo sound) is set up as a sequence of
@@ -15,7 +17,7 @@ referred to as 'short'), where 0 = silence, and +/-2**15 is loudest.
 
 DEBUG = True
 MAX_SOUND = 2**15
-REPS = 1 # Number of times each augmentation is run
+REPS = 2 # Number of times each augmentation is run
 
 def wavToShort(readBytes):
     """
@@ -42,8 +44,13 @@ def shortToWav(intList):
         augmentation method) back to bytes so it can be written out to a .wav file.
     """
     bitStr = b''
-    for i in intList:
-        bitStr += pack('h', i)
+    for index, i in enumerate(intList):
+        try:
+            bitStr += pack('h', i)
+        except:
+            print("BYTE TO SHORT FAILED!")
+            print("Value:", i, "at Index:", index)
+            break
     return bitStr
 
 def changeAmplitude(intList, ampChange):
@@ -54,14 +61,15 @@ def changeAmplitude(intList, ampChange):
         clipped if beyond MAX_SOUND value.
     Purpose: Apply change in amplitude uniformly across the sound wave.
     """
+    newList = []
     for index, value in enumerate(intList):
         newValue =  int(value * ampChange)
-        if newValue > MAX_SOUND:
-            newValue = MAX_SOUND
-        if newValue < -MAX_SOUND:
-            newValue = -MAX_SOUND
-        intList[index] = newValue
-    return intList
+        if newValue >= MAX_SOUND:
+            newValue = MAX_SOUND - 1
+        if newValue <= -MAX_SOUND:
+            newValue = -MAX_SOUND + 1
+        newList.append(newValue)
+    return newList
 
 def wobbleAmplitude(intList, ampChanges):
     """
@@ -77,66 +85,85 @@ def wobbleAmplitude(intList, ampChanges):
     """
     pass
 
-inWavFile = wave.open('coke.wav', 'rb')
-if DEBUG:
-    # Check input .wav file parameters.
-    # Expecting:
-    #   nchannels = 1
-    #   sampwidth = 2
-    #   framerate = 44100
-    #   nframes = variable
-    #   compType = 'NONE' or equivalent
-    #   compname = 'NONE' or equivalent
-    print(inWavFile.getparams())
-# Capture parameters for setting up output file.
-chn, swd, frt, nfr, cmt, cmn = inWavFile.getparams()
-# Read entire .wav file as bytes
-wavBytes = inWavFile.readframes(inWavFile.getnframes())
-inWavFile.close()
-intWav = wavToShort(wavBytes)
-if DEBUG:
-    # Make sure integer list length matches .wav number of frames
-    print("This should match nframes:", len(intWav))
-# Find maximum amplitude of wave as a percentage of maximum.
-maxAmp = max(abs(max(intWav)), abs(min(intWav))) / MAX_SOUND
-if DEBUG:
-    print("Max amplitude (as int):", max(abs(max(intWav)), abs(min(intWav))),
-        "Proportion of MAX:", maxAmp)
 
-# Start data augmentation process with uniform amplitude change
-# Amount and direction of change depends on maxAmp.
-ampFactors = []
-while len(ampFactors) < REPS:
-    if maxAmp < 0.33:
-        # If maxAmp is less than 0.33, multiply by a factor between 1 and 3
-        # Using powers of 2 to ensure exact decimal matching
-        factor = randrange(257, 769) / 256
-    elif maxAmp < 0.66:
-        # If maxAmp is between 0.33 and 0.66, multiply by 0.5 to 1.5
-        factor =  randrange(256, 769) / 512
-    else:
-        # If maxAmp is bigger than 0.66, multiply by 0.33 and 1
-        factor = randrange(257, 768) / 768
-    # Don't use the same factor twice
-    if factor not in ampFactors:
-        ampFactors.append(factor)
-if DEBUG:
-    print("Uniform Amplitude Factors:", ampFactors)
-for factor in ampFactors:
-    newIntWav = changeAmplitude(intWav, factor)
-    # Output to files.
+# Get a listing of input .wav files, convert stdout to string
+fileList = run(['ls', 'inputWavs'], capture_output=True).stdout.decode()
+fileList = fileList.split('\n')
+for inFileName in fileList:
+    if DEBUG: print('File List Entry:', inFileName)
+    if '.wav' in inFileName or '.WAV' in inFileName:
+        if DEBUG: print('File is wav.')
+        strippedFileName = inFileName.split('.wav')[0]
+        if DEBUG: print('Stripped File Name:', strippedFileName)
+        fileCounter = 0
+        inWavFile = wave.open(inFileName, 'rb')
+        if DEBUG:
+            # Check input .wav file parameters.
+            # Expecting:
+            #   nchannels = 1
+            #   sampwidth = 2
+            #   framerate = 44100
+            #   nframes = variable
+            #   compType = 'NONE' or equivalent
+            #   compname = 'NONE' or equivalent
+            print(inWavFile.getparams())
+        # Capture parameters for setting up output file.
+        chn, swd, frt, nfr, cmt, cmn = inWavFile.getparams()
+        # Read entire .wav file as bytes
+        wavBytes = inWavFile.readframes(inWavFile.getnframes())
+        inWavFile.close()
+        intWav = wavToShort(wavBytes)
+        if DEBUG:
+            # Make sure integer list length matches .wav number of frames
+            print("This should match nframes:", len(intWav))
+        # Find maximum amplitude of wave as a percentage of maximum.
+        maxAmp = max(abs(max(intWav)), abs(min(intWav))) / MAX_SOUND
+        if DEBUG: print("Max amplitude (as int):", max(abs(max(intWav)),
+            abs(min(intWav))), "Proportion of MAX:", maxAmp)
 
-# Next, augment by speeding up/slowing down the original by a factor up to 0.20
-spFactors = []
-while len(spFactors) < REPS:
-    factor = randrange(590, 615) / 512
-    if factor not in spFactors:
-        spFactors.append(factor)
-if DEBUG:
-    print("Speed Change Factors:", spFactors)
-for factor in spFactors:
-    outWavFile = wave.open('newCoke.wav', 'wb')
-    # Use captured paramters for setting up output file.
-    # Notice that we carry out the speed change by changing sample rate.
-    outWavFile.setparams((chn, swd, int(frt * factor), nfr, cmt, cmn))
-    outWavFile.writeframes(shortToWav(newIntWav))
+        # Start data augmentation process with uniform amplitude change
+        # Amount and direction of change depends on maxAmp.
+        ampFactors = []
+        while len(ampFactors) < REPS:
+            if maxAmp < 0.33:
+                # If maxAmp is less than 0.33, multiply by a factor between 1 and 3
+                # Using powers of 2 to ensure exact decimal matching
+                factor = randrange(257, 769) / 256
+            elif maxAmp < 0.66:
+                # If maxAmp is between 0.33 and 0.66, multiply by 0.5 to 1.5
+                factor =  randrange(256, 769) / 512
+            else:
+                # If maxAmp is bigger than 0.66, multiply by 0.33 and 1
+                factor = randrange(257, 768) / 768
+            # Don't use the same factor twice
+            if factor not in ampFactors:
+                ampFactors.append(factor)
+        if DEBUG: print("Uniform Amplitude Factors:", ampFactors)
+        for factor in ampFactors:
+            newIntWav = changeAmplitude(intWav, factor)
+            newFileName = 'outputWavs/' + strippedFileName + str(fileCounter) + '.wav'
+            if DEBUG: print('Writing file:', newFileName)
+            outWavFile = wave.open(newFileName, 'wb')
+            outWavFile.setparams((chn, swd, frt, nfr, cmt, cmn))
+            outWavFile.writeframes(shortToWav(newIntWav))
+            if DEBUG: print('Done.')
+            fileCounter += 1
+
+
+        # Next, augment by speeding up/slowing down the original by a factor up to 0.20
+        spFactors = []
+        while len(spFactors) < REPS:
+            factor = randrange(410, 615) / 512
+            if factor not in spFactors:
+                spFactors.append(factor)
+        if DEBUG: print("Speed Change Factors:", spFactors)
+        for factor in spFactors:
+            newFileName = 'outputWavs/' + strippedFileName + str(fileCounter) + '.wav'
+            if DEBUG: print('Writing file:', newFileName)
+            outWavFile = wave.open(newFileName, 'wb')
+            # Use captured paramters for setting up output file.
+            # Notice that we carry out the speed change by changing sample rate.
+            outWavFile.setparams((chn, swd, int(frt * factor), nfr, cmt, cmn))
+            outWavFile.writeframes(shortToWav(intWav))
+            if DEBUG: print('Done.')
+            fileCounter += 1
