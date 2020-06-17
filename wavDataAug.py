8968a39abb1bf5d80b23f4bd755884d6cd2247ac
TBSDrJ <marcus.jaiclin@bishops.com>
2020-06-17 13:10:15 -0700

    Import statements.

---
+++ wavDataAug.py
@@ -0,0 +1,5 @@
+# Using Python 3.7.4
+# Module for handling read/write of .wav sound files; in Standard Library
+import wave
+# Module for converting bytes to other types and back; in Standard Library
+from struct import pack, unpack
